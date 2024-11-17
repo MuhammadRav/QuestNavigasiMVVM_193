732f57c8c8cf4e6ce95e8e276e58ce7c8a8a05ac
MuhammadRav <muhammad.rezza.ft22@mail.umy.ac.id>
2024-11-17 09:37:10 +0700

    menambahkan tampil data nim, email dan nomor hp

--- app/src/main/java/com/example/minggu7/Model/Mahasiswa.kt
+++ app/src/main/java/com/example/minggu7/Model/Mahasiswa.kt
@@ -1,7 +1,12 @@
 package com.example.minggu7.Model
 
+import android.provider.ContactsContract.CommonDataKinds.Email
+
 data class Mahasiswa(
     val nama : String="",
+    val nim : String="",
     val gender : String="",
+    val email : String="",
+    val nomorHp : String="",
     val alamat : String=""
 )
--- app/src/main/java/com/example/minggu7/ui/view/TampilMahasiswaView.kt
+++ app/src/main/java/com/example/minggu7/ui/view/TampilMahasiswaView.kt
@@ -3,27 +3,52 @@ package com.example.minggu7.ui.view
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.minggu7.Model.Mahasiswa
 
 @Composable
 fun TampilMahasiswaView(
     mhs: Mahasiswa
 ){
-    Column(modifier = Modifier.fillMaxSize()) {
+    Column(modifier = Modifier.fillMaxSize()
+        .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(
+            "Data Anda",
+            fontWeight = FontWeight.Bold,
+            fontSize = 28.sp
+        )
+        Spacer(modifier = Modifier.padding(20.dp))
+
         TampilData(
             judul = "Nama",
             isinya = mhs.nama
         )
         TampilData(
+            judul = "NIM",
+            isinya = mhs.nim
+        )
+        TampilData(
             judul = "Gender",
             isinya = mhs.gender
+        )
+        TampilData(
+            judul = "Email",
+            isinya = mhs.email
+        )
+        TampilData(
+            judul = "Nomor HP",
+            isinya = mhs.nomorHp
         )
         TampilData(
             judul = "Alamat",
